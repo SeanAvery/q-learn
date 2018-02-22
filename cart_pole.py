b57419faf66c9803c985bc222ff40cf99fcc4910
Sean Pollock <seanpollock3344@gmail.com>
2018-02-22 13:56:22 -0500

    checking out incremental observations

--- cart_pole.py
+++ cart_pole.py
@@ -1,8 +1,16 @@
 import gym
-env = gym.make('MsPacman-v0')
-env.reset()
+env = gym.make('CartPole-v0')
 
-for _ in range(1000):
+for i in range(10):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        if done:
+            print('episode finished after {} timesteps'.format(t+1))
+            break
     env.render()
     env.step(env.action_space.sample())
 
